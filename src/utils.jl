45586e5f98de53673927136f67f8a2262528ad6d
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-01-15 15:46:19 +0100

    tests BFGS SR1

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,6 @@
 module M_utils
 
+	using LinearAlgebra
 	my_and = (a :: Bool,b :: Bool) -> (a && b)
 
 
@@ -7,18 +8,20 @@ module M_utils
 			BFGS(s, y, B)
 	Perform the BFGS update over the matrix B by using the vector s and y.
 	"""
-	BFGS(s :: Vector{Y}, y :: Vector{Y}, B :: Array{Y,2}, kwargs...) where Y <: Number = begin B_1=similar(B); BFGS!(s,y,B,B_1;kwargs...); B_1 end
+	BFGS(s :: Vector{Y}, y :: Vector{Y}, B :: Array{Y,2}; kwargs...) where Y <: Number = begin B_1=similar(B); BFGS!(s,y,B,B_1;kwargs...); B_1 end
 	BFGS(x :: Vector{Y}, x_1 :: Vector{Y}, g :: Vector{Y}, g_1 :: Vector{Y}, B :: Array{Y,2}; kwargs...) where Y <: Number = begin B_1=similar(B); BFGS!(x_1 - x, g_1 - g, B, B_1; kwargs...); B_1 end 
-	BFGS!(x :: Vector{Y}, x_1 :: Vector{Y}, g :: Vector{Y}, g_1 :: Vector{Y}, B :: Array{Y,2}, B_1 :: Array{Y,2}) where Y <: Number = BFGS!(x_1 - x, g_1 - g, B, B_1)
+	BFGS!(x :: Vector{Y}, x_1 :: Vector{Y}, g :: Vector{Y}, g_1 :: Vector{Y}, B :: Array{Y,2}, B_1 :: Array{Y,2}; kwargs...) where Y <: Number = BFGS!(x_1 - x, g_1 - g, B, B_1; kwargs...)
 	function BFGS!(s :: Vector{Y}, y :: Vector{Y}, B :: Array{Y,2}, B_1 :: Array{Y,2}; index=0, reset=4) where Y <: Number #Array that will store the next approximation of the Hessian
-		if (s * y > 0 ) # curvature condition
-			terme1 =  y * y' / dot(u,s)
+		if dot(s,y) > 0  # curvature condition
+			Bs = B * s 
+			terme1 =  y * y' / dot(y,s)
 			terme2 = Bs * Bs' / dot(Bs,s)
-			B_1 .= B .+ terme1 .+ terme2
+			B_1 .= B .+ terme1 .- terme2
 		elseif index < reset #
 			B_1 .= B 
 		else
-			B_1 .= reshape([ (i==j ? 1 : 0) for i = 1:n for j =1:n], n, n)
+			n = length(s)
+			B_1 .= reshape([ (i==j ? (Y)(1) : (Y)(0)) for i = 1:n for j =1:n], n, n)
 		end 
 	end
 
@@ -26,17 +29,18 @@ module M_utils
 			SR1(s, y, B)
 	Perform the BFGS update over the matrix B by using the vector s and y.
 	"""
-	SR1(s :: Vector{Y}, y :: Vector{Y}, B :: Array{Y,2}, kwargs...) where Y <: Number = begin B_1=similar(B); SR1!(s,y,B,B_1;kwargs...); B_1 end
+	SR1(s :: Vector{Y}, y :: Vector{Y}, B :: Array{Y,2}; kwargs...) where Y <: Number = begin B_1=similar(B); SR1!(s,y,B,B_1;kwargs...); B_1 end
 	SR1(x :: Vector{Y}, x_1 :: Vector{Y}, g :: Vector{Y}, g_1 :: Vector{Y}, B :: Array{Y,2}; kwargs...) where Y <: Number = begin B_1=similar(B); SR1!(x_1 - x, g_1 - g, B, B_1; kwargs...); B_1 end 
 	SR1!(x :: Vector{Y}, x_1 :: Vector{Y}, g :: Vector{Y}, g_1 :: Vector{Y}, B :: Array{Y,2}, B_1 :: Array{Y,2}) where Y <: Number = SR1!(x_1 - x, g_1 - g, B, B_1)	
 	function SR1!(s :: Vector{Y}, y :: Vector{Y}, B :: Array{Y,2}, B_1 :: Array{Y,2}; index=0, reset=4, ω = 1e-6) where Y <: Number
-		r = y .- dot(B,s)
-		if dot(s,r) > ω * norm(s,2) * norm(r,2)
-			B_1 .= B .+ (r * r')./ dot(s,r)
+		r = y .- B*s
+		if abs(dot(s,r)) > ω * norm(s,2) * norm(r,2)
+			B_1 .= B .+ ((r * r')./dot(s,r))
 		elseif index < reset #
-			B_1 .= B 
+			B_1 .= B 	
 		else
-			B_1 .= reshape([ (i==j ? 1 : 0) for i = 1:n for j =1:n], n, n)
+			n = length(s)
+			B_1 .= reshape([ (i==j ? (Y)(1) : (Y)(0)) for i = 1:n for j =1:n], n, n)
 		end 
 	end
 			
--- test/others/_include.jl
+++ test/others/_include.jl
@@ -1,1 +1,2 @@
+include("utils.jl")
 include("link.jl")
