ca64e114436174e92d460d05abcf49aab0f55811
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-04-12 14:45:53 -0300

    add documentation

--- src/methods/link.jl
+++ src/methods/link.jl
@@ -1,17 +1,17 @@
 module Link
   using LinearAlgebra, SparseArrays
 
-	using ..M_elt_mat
+  using ..M_elt_mat
   using ..M_part_mat, ..M_part_v
   using ..ModElemental_em, ..ModElemental_ev
   using ..ModElemental_pv, ..ModElemental_plom_bfgs, ..ModElemental_plom_sr1, ..ModElemental_plom, ..ModElemental_pm  
   using ..M_abstract_element_struct, ..M_abstract_part_struct
 
   export eplom_lbfgs_from_epv, eplom_lsr1_from_epv, eplom_lose_from_epv, epm_from_epv
-	export epv_from_eplom, epv_from_epm
+  export epv_from_eplom, epv_from_epm
   export mul_epm_epv, mul_epm_epv!, mul_epm_vector, mul_epm_vector!
-	export string_counters_iter, string_counters_total
-	
+  export string_counters_iter, string_counters_total
+  
   @inline epv_from_eplom(eplom) = epv_from_epm(eplom)
   """
       epv_from_epm(epm)
@@ -20,17 +20,17 @@ module Link
   function epv_from_epm(epm :: T) where T <: Part_mat{Y} where Y <: Number
     N = get_N(epm)
     n = get_n(epm)
-    eev_set = Vector{Elemental_elt_vec{Y}}(undef,N)
+    eev_set = Vector{Elemental_elt_vec{Y}}(undef, N)
     for i in 1:N
-      eesi = get_ee_struct(epm,i)
+      eesi = get_ee_struct(epm, i)
       indices = get_indices(eesi)
       nie = get_nie(eesi)
-      eev_set[i] = Elemental_elt_vec{Y}(rand(Y,nie), indices, nie)
+      eev_set[i] = Elemental_elt_vec{Y}(rand(Y, nie), indices, nie)
     end 
     component_list = M_abstract_part_struct.get_component_list(epm)
-    v = rand(Y,n)
+    v = rand(Y, n)
     perm = [1:n;]
-    epv = Elemental_pv{Y}(N, n, eev_set, v, component_list,perm)
+    epv = Elemental_pv{Y}(N, n, eev_set, v, component_list, perm)
     return epv
   end
   
@@ -40,11 +40,11 @@ module Link
   Each elemental element matrix is set with an identity matrix.
   """
   function epm_from_epv(epv :: T) where T <: Elemental_pv{Y} where Y <: Number
-		N = get_N(epv)
+    N = get_N(epv)
     n = get_n(epv)
-    eelom_indices_set = Vector{Vector{Int}}(undef,N)
+    eelom_indices_set = Vector{Vector{Int}}(undef, N)
     for i in 1:N
-      eesi = get_ee_struct(epv,i)
+      eesi = get_ee_struct(epv, i)
       indices = get_indices(eesi)
       eelom_indices_set[i] = indices    
     end 
@@ -60,9 +60,9 @@ module Link
   function eplom_lbfgs_from_epv(epv :: T) where T <: Elemental_pv{Y} where Y <: Number
     N = get_N(epv)
     n = get_n(epv)
-    eelom_indices_set = Vector{Vector{Int}}(undef,N)
+    eelom_indices_set = Vector{Vector{Int}}(undef, N)
     for i in 1:N
-      eesi = get_ee_struct(epv,i)
+      eesi = get_ee_struct(epv, i)
       indices = get_indices(eesi)
       eelom_indices_set[i] = indices    
     end 
@@ -71,34 +71,34 @@ module Link
   end
 
   """
-			eplom_lsr1_from_epv(epm)
+      eplom_lsr1_from_epv(epm)
   Create an elemental partitioned linear operator matrix with the same partitioned structure than `epv`.
   Each elemental element linear operator is set with an LSR1 operator.
   """
   function eplom_lsr1_from_epv(epv :: T) where T <: Elemental_pv{Y} where Y <: Number
     N = get_N(epv)
     n = get_n(epv)
-    eelom_indices_set = Vector{Vector{Int}}(undef,N)
+    eelom_indices_set = Vector{Vector{Int}}(undef, N)
     for i in 1:N
-      eesi = get_ee_struct(epv,i)
+      eesi = get_ee_struct(epv, i)
       indices = get_indices(eesi)
       eelom_indices_set[i] = indices    
     end 
     eplom = identity_eplom_LSR1(eelom_indices_set, N, n; T=Y)
     return eplom
   end
-	
-	"""
-			eplom_lose_from_epv(epm)
+  
+  """
+      eplom_lose_from_epv(epm)
   Create an elemental partitioned linear operator matrix with the same partitioned structure than `epv`.
   Each elemental element linear operator is set with an LSR1 operator.
   """
   function eplom_lose_from_epv(epv :: Elemental_pv{T}) where T <: Number
     N = get_N(epv)
     n = get_n(epv)
-    eelom_indices_set = Vector{Vector{Int}}(undef,N)
+    eelom_indices_set = Vector{Vector{Int}}(undef, N)
     for i in 1:N
-      eesi = get_ee_struct(epv,i)
+      eesi = get_ee_struct(epv, i)
       indices = get_indices(eesi)
       eelom_indices_set[i] = indices    
     end 
@@ -127,7 +127,7 @@ module Link
   """	
   function mul_epm_vector!(res :: Vector{Y}, epm :: T, x :: Vector{Y}) where T <: Part_mat{Y} where Y <: Number
     epv = epv_from_epm(epm)
-    mul_epm_vector!(res,epm,epv,x)
+    mul_epm_vector!(res, epm, epv, x)
   end
 
   """
@@ -136,8 +136,8 @@ module Link
   The result is stored in the vector `res`.
   """	
   function mul_epm_vector!(res :: Vector{Y}, epm :: T, epv :: Elemental_pv{Y}, x :: Vector{Y}) where T <: Part_mat{Y} where Y <: Number
-    epv_from_v!(epv,x)
-    mul_epm_epv!(epv,epm,epv)
+    epv_from_v!(epv, x)
+    mul_epm_epv!(epv, epm, epv)
     build_v!(epv)
     res .= get_v(epv)
   end 
@@ -158,47 +158,53 @@ module Link
   The result is stored in the elemental partitioned vector `epv_res`.
   """		
   function mul_epm_epv!(epv_res :: Elemental_pv{Y}, epm :: T, epv :: Elemental_pv{Y}) where T <: Part_mat{Y} where Y <: Number
-    full_check_epv_epm(epm,epv) || error("Structure differ epm/epv")
+    full_check_epv_epm(epm, epv) || error("Structure differ epm/epv")
     N = get_N(epm)
     for i in 1:N
-      Bie = get_ee_struct_Bie(epm,i)
+      Bie = get_ee_struct_Bie(epm, i)
       vie = get_eev_value(epv, i)
-      set_eev!(epv_res, i,Bie*vie)
+      set_eev!(epv_res, i, Bie*vie)
     end
   end 
 
-	function string_counters_iter(pm :: T) where T <: Part_mat
-		epm_vectors = get_ee_struct(pm)
-		counters = (epm -> epm.counter).(epm_vectors)
-		@show length(counters)
-		update = 0
-		untouch = 0
-		reset = 0
-		for counter in counters
-			(up, un, re) = iter_info(counter)
-			update += (up>0 ? 1 : 0)
-			untouch += (un>0 ? 1 : 0)
-			reset += (re>0 ? 1 : 0)
-		end 
-		N = get_N(pm)
-		println("Partitioned update ", T, " with ", N, " elements;", " update: ", update, ", untouch: ", untouch, ", reset: ", reset)
-	end
+  """
+      string_counters_iter(pm)
+  Produce a string that summarize the partitioned update applied onto `pm` at the last iterate.
+  """
+  function string_counters_iter(pm :: T) where T <: Part_mat
+    epm_vectors = get_ee_struct(pm)
+    counters = (epm -> epm.counter).(epm_vectors)
+    update = 0
+    untouch = 0
+    reset = 0
+    for counter in counters
+      (up, un, re) = iter_info(counter)
+      update += (up>0 ? 1 : 0)
+      untouch += (un>0 ? 1 : 0)
+      reset += (re>0 ? 1 : 0)
+    end 
+    N = get_N(pm)
+    println("Partitioned update ", T, " with ", N, " elements;", " update: ", update, ", untouch: ", untouch, ", reset: ", reset)
+  end
 
-	function string_counters_total(pm :: T) where T <: Part_mat
-		epm_vectors = get_ee_struct(pm)
-		counters = (epm -> epm.counter).(epm_vectors)
-		update = 0
-		untouch = 0
-		reset = 0
-		for counter in counters
-			(up, un, re) = total_info(counter)
-			update += (up>0 ? 1 : 0)
-			untouch += (un>0 ? 1 : 0)
-			reset += (re>0 ? 1 : 0)
-		end 
-		N = get_N(pm)
-		println("Partitioned update ", T, " with ", N, " elements;", " update: ", update, ", untouch: ", untouch, ", reset: ", reset)
-	end
-
+  """
+      string_counters_total(pm)
+  Produce a string that summarize the partitioned update applied onto `pm` from its allocation.
+  """
+  function string_counters_total(pm :: T) where T <: Part_mat
+    epm_vectors = get_ee_struct(pm)
+    counters = (epm -> epm.counter).(epm_vectors)
+    update = 0
+    untouch = 0
+    reset = 0
+    for counter in counters
+      (up, un, re) = total_info(counter)
+      update += up
+      untouch += un
+      reset += re
+    end 
+    N = get_N(pm)
+    println("Partitioned update ", T, " with ", N, " elements;", " update: ", update, ", untouch: ", untouch, ", reset: ", reset)
+  end
 
 end 
